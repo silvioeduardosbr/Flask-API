0ce4e06e4baa0cecf556189b31ef3d37a04ff165
Silvio Eduardo <silvioeduardosbr@gmail.com>
2022-11-14 16:33:35 -0300

    QSASA

--- Flask/tarefas_api/app.py
+++ Flask/tarefas_api/app.py
@@ -1,10 +1,61 @@
-from flask import Flask, jsonify
+import json
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/res')
-def responsavel():
-    return jsonify({'nome':'Silvio'})
+tarefas = [
+    {
+        'id': 0,
+        'responsavel': 'Silvio',
+        'tarefa': 'Desenvolver metodo GET',
+        'status': 'concluido'
+    },
+
+    {
+        'id': 1,
+        'responsavel': 'Eduardo',
+        'tarefa': 'Desenvolver metodo POST',
+        'status': 'pendente'
+    },
+    
+    {
+        'id': 2,
+        'responsavel': 'Sales',
+        'tarefa': 'Desenvolver metodo PUT',
+        'status': 'pendente'
+    }
+]
+
+
+@app.route('/tarefa/<int:id>/', methods = ['GET','PUT','DELETE'])
+def tarefa_by_id(id):
+    if request.method == 'GET':
+        for i in tarefas:
+            if i['id'] == id:
+                return jsonify(i)
+        return jsonify({'mensagem': 'tarefa inexistente'})
+           
+    elif request.method == 'PUT':
+        dados = json.loads(request.data)
+        tarefas[id] = dados
+        return jsonify(dados)
+    elif request.method == 'DELETE':
+        for i in tarefas:
+            if i['id'] == id:
+                tarefas.pop(id) 
+                return jsonify({'status':'sucesso', 'mensagem':'Registro Excluido!'})
+        return jsonify({'mensagem': 'tarefa inexistente'})
+
+@app.route('/tarefas', methods = ['POST'])
+def tarefa():
+    
+    data = request.json
+    data['id'] = len(tarefas)
+    tarefas.append(data)
+
+    return jsonify({'mensagem':'tarefa criada'})
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
